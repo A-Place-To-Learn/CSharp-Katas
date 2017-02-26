dc2ace39b8fd3fb097c5e4ce619b7ef501f79139
CJ Taylor <cjtayl05@gmail.com>
2017-02-26 15:32:20 -0500

    Created Visual Studio project, introduced unit tests (left some intentionally failing for @Stormxftw to run down why they're failing :) ).

---
+++ Kata.MonthChecker.Tests/ConsoleOutputTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata.MonthChecker.Tests
+{
+    [TestClass]
+    public class ConsoleOutputTests
+    {
+        private string GetConsoleTextAfterInput(string input)
+        {
+            string consoleResult;
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                using (var sr = new StringReader($"{input}{Environment.NewLine}"))
+                {
+                    Console.SetIn(sr);
+
+                    Program.Main(new string[] { });
+
+                    consoleResult = sw.ToString();
+                }
+            }
+
+            return consoleResult;
+        }
+
+        [TestMethod]
+        public void ItShouldProvideUserWithMessageToInsertInput()
+        {
+            var expected = "Please enter the month in numerical value (1-12)";
+
+            var result = GetConsoleTextAfterInput("");
+
+            Assert.IsTrue(result.Contains(expected));
+        }
+
+        [TestMethod]
+        public void ItShouldReturnMessageWhenInputIsNotNumberOfMonth()
+        {
+            var expected = "Please close the program, run the program again, and input number between 1-12";
+            var consoleInput = "0";
+
+            var result = GetConsoleTextAfterInput(consoleInput);
+
+            Assert.IsTrue(result.Contains(expected));
+        }
+
+        [TestMethod]
+        public void ItShouldContainASeasonMessageWhenMonthIsValid()
+        {
+            var expected = "Your month is a Winter month.";
+            var consoleInput = "1";
+
+            var result = GetConsoleTextAfterInput(consoleInput);
+
+            Assert.IsTrue(result.Contains(expected));
+        }
+    }
+}
---
+++ Kata.MonthChecker/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Kata.MonthChecker
+{
+    public class Program
+    {
+        enum month { January, Febuary, March, Apirl, May, June, July, Augest, September, October, November, December };
+        public static void Main(string[] args)
+        {
+            string[] months = { "January", "Febuary", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December" };
+            Console.WriteLine("Please enter the month in numerical value (1-12)");
+            Console.ReadLine();
+
+
+
+            string input = Console.ReadLine();
+            int result;
+            if (!int.TryParse(input, out result))
+            {
+
+                if (result > 1 && result < 13)
+
+                {
+
+                    Console.WriteLine("Please close the program, run the program again, and input  number between 1-12");
+                    Console.ReadLine();
+
+                }
+                else
+                {
+
+                    switch (result)
+                    {
+                        case 1:
+                            Console.WriteLine("Your month is a Winter month.");
+
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Your month is a Winter month");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Your month is a Spring month");
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Your month is a Spring month.");
+                            break;
+                        case 5:
+                            Console.WriteLine("Your month is a Spring month.");
+                            break;
+                        case 6:
+                            Console.WriteLine("Your month is a Summer month.");
+                            break;
+                        case 7:
+                            Console.WriteLine("Your month is a Summer month.");
+                            break;
+                        case 8:
+                            Console.WriteLine("Your month is a Summer month.");
+                            break;
+                        case 9:
+                            Console.WriteLine("Your month is a Fall month.");
+                            break;
+                        case 10:
+                            Console.WriteLine("Your month is a Fall month.");
+                            break;
+                        case 11:
+                            Console.WriteLine("Your month is a Fall month.");
+                            break;
+                        case 12:
+                            Console.WriteLine("Your month is a Winter month.");
+                            break;
+                    }
+                }
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
+
+
